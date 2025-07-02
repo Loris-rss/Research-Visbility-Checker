d98fea6e8f03c100913f9634cd70ae49dde54e2a
LynxSheep <115998541+LynxSheepOG@users.noreply.github.com>
2025-07-02 15:47:09 +0200

    Fix file download encoding and Excel writer in data export
    Updated CSV export to encode data as UTF-8 bytes for compatibility with Streamlit's download button. Changed Excel export to use 'xlsxwriter' engine and ensured the output buffer is properly handled for download. Removed unnecessary success messages after downloads.

--- pages/3_st_show_donnee.py
+++ pages/3_st_show_donnee.py
@@ -30,22 +30,39 @@ for tab, (db_name, df) in zip(tabs, st.session_state["databases"].items()):
         right_col = "".join([col for col in df.columns if col in date_list_col])
         
         csv, xlsx = st.columns(2)
+        
         with csv:
-            csv_data = df.to_csv(index=False)
-            if st.download_button("Télécharger les données en csv", csv_data, file_name=f"{db_name}.csv", mime="text/csv"):
-                st.success("Les données ont été téléchargées avec succès.")
+            csv_data = df.to_csv(index=False).encode("utf-8")
+
+            # Créer le bouton de téléchargement
+            st.download_button(
+                label="Télécharger les données en csv",
+                data=csv_data,
+                file_name=f"{db_name}.csv",
+                mime="text/csv"
+            )
+        
         with xlsx:
             output = BytesIO()
-            df.to_excel(output, index=False, engine='openpyxl')
-            xlsx_data = output.getvalue()
-            
-            if st.download_button("Télécharger les données en excel", xlsx_data, file_name=f"{db_name}.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"):
-                st.success("Les données ont été téléchargées avec succès.")
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                df.to_excel(writer, index=False, sheet_name='Sheet1')
+
+            # Repositionner le curseur du buffer au début
+            output.seek(0)
+
+            # Télécharger via Streamlit
+            st.download_button(
+                label="Télécharger les données en excel",
+                data=output,
+                file_name=f"{db_name}.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+
 
         st.divider()
 
         st.markdown("## Distribution temporelle des publications")
-        # Distribution des publications par anné
+        # Distribution des publications par année
         with st.expander("Distribution temporelle des publications", expanded=False):
             st.markdown("""
             Cette section présente la répartition des publications scientifiques par année. Cela permet de visualiser l'évolution du volume de publications au fil du temps pour le chercheur sélectionné.
