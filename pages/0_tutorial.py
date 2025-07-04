d3f75c1233aca2fc6d82915be08a1d4c3477b234
LynxSheep <115998541+LynxSheepOG@users.noreply.github.com>
2025-07-04 15:57:10 +0200

    Add Scopus tutorial section with images
    Introduced a new section in the tutorial for Scopus, including step-by-step instructions and illustrative images for exporting research articles. Added three new images to support the Scopus export process.

--- pages/0_tutorial.py
+++ pages/0_tutorial.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.header("Tutoriel d'utililisation")
 
+st.subheader("Web Of Science")
 with st.expander("Récupérer les articles de recherche dans Web Of Science :", expanded=False):
     st.write("Etape 1  - Aller sur le profile du chercheur.")
     st.write("Etape 2 - Cliquer sur le nombre de publications pour afficher tous les articles de recherche du chercheur.")
@@ -16,10 +17,24 @@ with st.expander("Récupérer les articles de recherche dans Web Of Science :", 
     # st.write("Dans le cas où vous avez plusieurs fichiers à télécharger, il faut les fusionner en un seul fichier.")
     st.write("Etape 5 - Une fois le fichier téléchargé, il faut le faire changer l'extension de xls à xlsx.")
 
+st.divider()
+st.subheader("Scopus")
+
 with st.expander("Récupérer l'identifiant Scopus :", expanded=False):
     st.write("Etape 1 - Aller sur le profile Scopus du chercheur.")
     st.image(r"img/Scopus_id.png",width=1000)
     st.write("Etape 2 - Copier-Coller l'identifiant Scopus et l'entrer dans le bon champs.")
+
+with st.expander("Récupérer article de recherche dans Scopus :", expanded=False):
+    st.write("Etape 1 - Aller sur le profile Scopus du chercheur. Puis cliquer sur le bouton 'Export All'.")
+    st.image(r"img/export scopus article.png",width=1000)
+    st.write("Etape 2 - Sélectionner ensuite l'extension 'CSV'.")
+    st.image(r"img/enregistrement csv scopus article.png",width=1000)
+    st.write("Etape 3 - Puis cliquer sur le bouton 'Export' en bas à droite.")
+    st.image(r"img/Export all Scopus.png",width=1000)
+
+st.divider()
+st.subheader("Orcid")
 
 with st.expander("Récupérer l'URL orcID du chercheur :", expanded=False):
     st.write("Etape 1 - Aller sur le profile orcID du chercheur.")
